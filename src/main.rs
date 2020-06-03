24a30b4df5d28da573728c8bd792a24512bc186e
Andrea Tupini <tupini07@gmail.com>
2020-06-03 11:19:35 -0600

    basic table printing

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,17 @@
-// To print tables use this library
-// https://github.com/phsym/prettytable-rs
 #[macro_use]
 extern crate prettytable;
 extern crate dirs;
 extern crate regex;
 
-use prettytable::{Cell, Row, Table};
+use prettytable::{
+    format::{self, TableFormat},
+    Cell, Row, Table,
+};
 use regex::Regex;
+use std::cmp::Ordering;
 use std::{env, fs, path};
 
 fn main() {
-    // Create the table
-    let mut table = Table::new();
-
-    // Add a row per time
-    table.add_row(row!["ABC", "DEFG", "HIJKLMN"]);
-    table.add_row(row!["foobar", "bar", "foo"]);
-    // A more complicated way to add a row:
-    table.add_row(Row::new(vec![
-        Cell::new("foobar2"),
-        Cell::new("bar2"),
-        Cell::new("foo2"),
-    ]));
-
-    // Print the table to stdout
-    table.printstd();
-
     // read a file
     let file_path = dirs::home_dir()
         .unwrap()
@@ -36,6 +22,7 @@ fn main() {
 
     #[derive(Debug)]
     struct I3Binding {
+        binding_type: String,
         binding: String,
         command: String,
     }
@@ -44,22 +31,65 @@ fn main() {
 
     // TODO: "Exec" modifiers are currently being added as part of the command
     let capture_exec =
-        Regex::new(r"bind\w+ (?P<binding>(\w|\$|\+)+) (exec )?(?P<command>.+)").unwrap();
+        Regex::new(r"(?P<binding_type>bind\w+) (?P<binding>(\w|\$|\+)+) (exec )?(?P<command>.+)")
+            .unwrap();
 
-    let filtered_contents: Vec<I3Binding> = raw_contents
+    let mut filtered_contents: Vec<I3Binding> = raw_contents
         .split("\n")
         .filter(|e| re.is_match(e))
         .map(|e: &str| e.trim())
         .map(|e: &str| {
             let caps = capture_exec.captures(e).unwrap();
+            let binding_type = match caps.name("binding_type").unwrap().as_str() {
+                "bindsym" => "Symbol",
+                "bindcode" => "Code",
+                _ => "Unknown",
+            };
 
             I3Binding {
+                binding_type: binding_type.to_string(),
                 binding: caps.name("binding").unwrap().as_str().to_string(),
                 command: caps.name("command").unwrap().as_str().to_string(),
             }
         })
         .collect();
 
-    // filtered_contents.iter()
-    println!("file contents:\n{:?}", filtered_contents);
+    filtered_contents.sort_by(|e, i| {
+        if e.binding > i.binding {
+            Ordering::Greater
+        } else {
+            Ordering::Less
+        }
+    });
+
+    // Create the table
+    let mut table = Table::new();
+
+    // Add a row per time
+    table.set_titles(row!["Type", "Binding", "Command"]);
+    for xx in filtered_contents {
+        table.add_row(row![xx.binding_type, xx.binding, xx.command]);
+    }
+    // table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
+    table.set_format(
+        format::FormatBuilder::new()
+            .column_separator('║')
+            .borders('║')
+            .separators(
+                &[format::LinePosition::Top],
+                format::LineSeparator::new('═', '╦', '╔', '╗'),
+            )
+            .separators(
+                &[format::LinePosition::Bottom],
+                format::LineSeparator::new('═', '╩', '╚', '╝'),
+            )
+            .separators(
+                &[format::LinePosition::Title],
+                format::LineSeparator::new('─', '╫', '╟', '╢'),
+            )
+            .build(),
+    );
+
+    // Print the table to stdout
+    table.printstd();
 }
