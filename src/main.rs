e09bf16723a22074f455e910fe0fc206d8d1490f
Andrea Tupini <tupini07@gmail.com>
2020-06-04 08:01:07 -0600

    consider categories for bindings

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use prettytable::{
 };
 use regex::Regex;
 use std::cmp::Ordering;
+use std::collections::HashMap;
 use std::io::Read;
 use std::{env, fs, io, path};
 
@@ -28,51 +29,81 @@ fn main() {
         command: String,
     }
 
-    let re = Regex::new(r"^\s*bind").unwrap();
+    let re = Regex::new(r"^\s*(bind|.*(C|c)ategory:)").unwrap();
 
     // TODO: "Exec" modifiers are currently being added as part of the command
     let capture_exec =
         Regex::new(r"(?P<binding_type>bind\w+) (?P<binding>(\w|\$|\+)+) (exec )?(?P<command>.+)")
             .unwrap();
+    let capture_category = Regex::new(r".*(C|c)ategory: (?P<category>.+)").unwrap();
 
-    let mut filtered_contents: Vec<I3Binding> = raw_contents
+    let mut filtered_lines: Vec<&str> = raw_contents
         .split("\n")
         .filter(|e| re.is_match(e))
         .map(|e: &str| e.trim())
-        .map(|e: &str| {
-            let caps = capture_exec.captures(e).unwrap();
+        .collect();
+
+    let mut last_category = "default";
+    let mut bindings_map: HashMap<&str, Vec<I3Binding>> = HashMap::new();
+    bindings_map.insert(last_category, Vec::new());
+
+    for line in filtered_lines {
+        if capture_category.is_match(line) {
+            let caps = capture_category.captures(line).unwrap();
+            last_category = caps.name("category").unwrap().as_str();
+
+            if !bindings_map.contains_key(last_category) {
+                bindings_map.insert(last_category, Vec::new());
+            }
+        } else if capture_exec.is_match(line) {
+            let caps = capture_exec.captures(line).unwrap();
             let binding_type = match caps.name("binding_type").unwrap().as_str() {
                 "bindsym" => "Symbol",
                 "bindcode" => "Code",
                 _ => "Unknown",
             };
-
-            I3Binding {
-                binding_type: binding_type.to_string(),
-                binding: caps.name("binding").unwrap().as_str().to_string(),
-                command: caps.name("command").unwrap().as_str().to_string(),
-            }
-        })
-        .collect();
-
-    filtered_contents.sort_by(|e, i| {
-        if e.binding > i.binding {
-            Ordering::Greater
-        } else {
-            Ordering::Less
+            bindings_map
+                .get_mut(last_category)
+                .unwrap()
+                .push(I3Binding {
+                    binding_type: binding_type.to_string(),
+                    binding: caps.name("binding").unwrap().as_str().to_string(),
+                    command: caps.name("command").unwrap().as_str().to_string(),
+                });
         }
-    });
-
-    // Create the table
-    let mut table = Table::new();
-
-    // Add a row per time
-    table.set_titles(row!["Type", "Binding", "Command"]);
-    for xx in filtered_contents {
-        table.add_row(row![xx.binding_type, xx.binding, xx.command]);
     }
-    // table.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
-    table.set_format(
+
+    let mut main_table = Table::new();
+    main_table.set_titles(row!["Category", "Actual Binding"]);
+
+    let mut sorted_vec: Vec<_> = bindings_map.iter_mut().collect();
+    sorted_vec.sort_by(|a,b| a.0.cmp(b.0));
+
+    for (&category, bindings_for_category) in sorted_vec.iter_mut() {
+        if bindings_for_category.len() == 0 {
+            continue;
+        }
+
+        let mut category_table = Table::new();
+
+        bindings_for_category.sort_by(|e, i| {
+            if e.binding > i.binding {
+                Ordering::Greater
+            } else {
+                Ordering::Less
+            }
+        });
+
+        for element in bindings_for_category.iter() {
+            category_table.add_row(row![element.binding_type, element.binding, element.command]);
+        }
+
+        category_table.set_format(*prettytable::format::consts::FORMAT_CLEAN);
+
+        main_table.add_row(row![category, category_table]);
+    }
+
+    main_table.set_format(
         format::FormatBuilder::new()
             .column_separator('║')
             .borders('║')
@@ -85,14 +116,13 @@ fn main() {
                 format::LineSeparator::new('═', '╩', '╚', '╝'),
             )
             .separators(
-                &[format::LinePosition::Title],
+                &[format::LinePosition::Title, format::LinePosition::Intern],
                 format::LineSeparator::new('─', '╫', '╟', '╢'),
             )
             .build(),
     );
 
-    // Print the table to stdout
-    table.printstd();
+    main_table.printstd();
 
     // wait for enter before exiting
     let _ = io::stdin().read(&mut [0u8]).unwrap();
