68c55dd63329fd820ed0ba9e44b6e2eecaee5a8d
AASanchezA <sanchezandres@siemens.com>
2022-01-05 16:45:25 +0100

    feat: extract type from binding and delimiter option for csv
    - extract binding type from bindsym e.g. |exec|mode|split|move...
    - add delimiter option for csv file

--- src/drawers/csv_drawer.rs
+++ src/drawers/csv_drawer.rs
@@ -2,9 +2,9 @@ use crate::config_reader::I3Binding;
 use std::collections::HashMap;
 use std::io;
 
-pub fn draw(bindings: HashMap<String, Vec<I3Binding>>) {
+pub fn draw(bindings: HashMap<String, Vec<I3Binding>>, delimiter: char) {
     let mut wtr = csv::WriterBuilder::new()
-        .delimiter(b',')
+        .delimiter(delimiter as u8)
         .quote_style(csv::QuoteStyle::NonNumeric)
         .from_writer(io::stdout());
 
